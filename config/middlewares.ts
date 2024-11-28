35fbefe13a2cef902e511ba33986b4419a562b97
Vinicius Miazaki <vinicius@cinza.io>
2024-11-28 03:47:22 -0800

    Initial commit: Memorylog backend setup with Strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,6 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,28 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.CLOUDFRONT_URL,
+            process.env.AWS_URL
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.CLOUDFRONT_URL,
+            process.env.AWS_URL
+          ],
+        },
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_BUCKET_NAME,
+        },
+      },
+      actionOptions: {
+        upload: {
+          ACL: null,
+        },
+        uploadStream: {
+          ACL: null,
+        },
+      }
+    },
+  },
+});
